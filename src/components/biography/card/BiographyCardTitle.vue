c2a7ba31fecee9dc887c637c6e8bafa8ae57c667
GadzhievSA <said.gadzhiev@id-mt.ru>
2019-01-30 07:55:33 +0300

    Добавил автора и переделал проверку является автобиографией или нет

--- src/components/biography/card/BiographyCardTitle.vue
+++ src/components/biography/card/BiographyCardTitle.vue
@@ -3,7 +3,12 @@
     <span v-for="(category, index) in categories" :key="index">
       <router-link class="bibliographya-a" :to="'/category/' + category">{{ category }}</router-link>&nbsp;&nbsp;
     </span>
-    <h2 class="word-break">{{ fullName }}
+    <div v-if="_isAutobiography">
+    <span class="word-break">
+      Автор: {{ _creatorName }}
+    </span>
+    </div>
+    <h2 class="word-break">{{ _fullName }}
     </h2>
     <v-icon v-if="_isMarked" small color="blue darken-3" class="pl-1">mdi-check-decagram</v-icon>
     <biography-card-menu v-bind="$attrs" v-on="$listeners"/>
@@ -18,7 +23,9 @@ export default {
   name: 'BiographyCardTitle',
   inheritAttrs: false,
   props: {
-    isAutobiography: Boolean,
+    userId: {
+      type: Number
+    },
     firstName: {
       type: String,
       default: ''
@@ -39,19 +46,29 @@ export default {
       default: function () {
         return []
       }
+    },
+    creatorBiography: {
+      type: Object,
+      required: true
     }
   },
   computed: {
     _isMarked () {
-      return this.isAutobiography && this.publishStatus === PUBLISH_STATUS.PUBLISHED
+      return this.userId && this.publishStatus === PUBLISH_STATUS.PUBLISHED
     },
-    fullName () {
+    _fullName () {
       let full = this.lastName + ' ' + this.firstName
       if (this.middleName) {
         return full + ' ' + this.middleName
       }
 
       return full
+    },
+    _creatorName () {
+      return this.creatorBiography.firstName + ' ' + this.creatorBiography.lastName
+    },
+    _isAutobiography () {
+      return !!this.userId
     }
   },
   components: {
