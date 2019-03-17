364ac7a3cc4cc38517abef17565dff6c32dc957f
GadzhievSA <said.gadzhiev@id-mt.ru>
2019-03-17 14:50:46 +0300

    Фикс переход на корневую страницу

--- src/router.js
+++ src/router.js
@@ -174,11 +174,18 @@ let router = new Router({
     },
     {
       path: '/',
-      alias: '/signIn',
-      component: SignIn,
-      beforeEnter: ifNotAuthenticated,
-      meta: {
-        layout: LAYOUTS.AUTH_LAYOUT
+      name: 'main',
+      component: CategoriesList,
+      beforeEnter: function (to, from, next) {
+        function proceed () {
+          if (store.getters.isAuthenticated) {
+            next()
+          } else {
+            next('/signIn')
+          }
+        }
+
+        waitForAccount(proceed)
       }
     },
     {
