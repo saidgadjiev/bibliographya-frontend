dc51c4d6c702416b0e0efe6b3abb704cc6698256
GadzhievSA <said.gadzhiev@id-mt.ru>
2019-06-21 00:08:07 +0300

    Страница для соц. сетей

--- src/views/Social.vue
+++ src/views/Social.vue
@@ -1,17 +1,30 @@
 <template>
-  <v-card tile>
-    <v-card-title style="justify-content: center">
-      Наши сообщества в соц. сетях
+  <v-card>
+    <v-card-title primary-title style="justify-content: center">
+      <div class="text-xs-center">
+        <h3 style="color: #455A64">Наши сообщества в соц. сетях.</h3>
+        <h2>Присоединяйся!</h2>
+      </div>
     </v-card-title>
     <v-card-actions>
-      <v-btn
-        left
-        color="primary"
-        class="white--text"
-        :href="tgLink"
-      >
-        Присоединиться
-      </v-btn>
+      <v-layout row wrap justify-center align-center>
+        <v-flex xs12 md4>
+          <v-btn color="primary" block :href="tgLink">
+            <v-icon left>
+              fab fa-telegram-plane
+            </v-icon>
+            Телеграмм
+          </v-btn>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-btn color="green darken-3" class="white--text" block href="https://facebook.com/bibliographya" target="_blank">
+            <v-icon left>
+              fab fa-facebook-f
+            </v-icon>
+            Facebook
+          </v-btn>
+        </v-flex>
+      </v-layout>
     </v-card-actions>
   </v-card>
 </template>
@@ -25,9 +38,6 @@ export default {
     return {
       tgLink: TG_LINK
     }
-  },
-  mounted () {
-    window.location.href = TG_LINK
   }
 }
 </script>
